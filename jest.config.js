8487c8b5d810a00dc31b9871d1cbc3531ce944de
tallyb <tally.barak@gmail.com>
2019-05-07 16:36:28 +0300

    choe(app): Clean jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,3 @@
-const path = require('path');
-
-const rootDir = '@stencil/core';
-const distDir = path.join(rootDir, 'dist');
-// const testingDir = path.join(rootDir, 'testing');
-
 const config = {
   preset: '@stencil/core/testing', 
 }
