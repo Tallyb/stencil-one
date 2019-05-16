e2b29f8ee1a7606c2921eca3a567072a8a8860e7
tallyb <tally.barak@gmail.com>
2019-05-16 15:41:14 +0300

    chore(app): Update stencil one

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,13 +1,13 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'mycomponent',
+  namespace: 'myspace',
   outputTargets:[
     { type: 'dist' },
-    { type: 'docs' },
-    // {
-    //   type: 'www',
-    //   serviceWorker: null // disable service workers
-    // }
+    { type: 'docs-readme' },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
   ]
 };
