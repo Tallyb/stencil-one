3c129c504e3ac38eb2f14f96302ff7ee00d09955
tallyb <tally.barak@gmail.com>
2021-10-07 15:17:12 +0300

    Add mock test

---
+++ src/components/component/component-with-mock.spec.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-useless-escape */
+import { newSpecPage } from '@stencil/core/testing';
+import { h } from '@stencil/core';
+
+const mockFormat = jest.fn();
+jest.mock('../../utils', () => { //import here must be exactly the same as in the tested file
+  return {
+    format: mockFormat
+  };
+});
+//component import must come after the mock! 
+import { MyComponent } from './component';
+
+describe('it should mock external file', () => {
+  it('should format mock value', async () => {
+    mockFormat.mockImplementation(() => 'Tally Barak');
+    const { root } = await newSpecPage({
+      components: [MyComponent],
+      template: () => (
+        <my-component first="Hello" last="World">Some Text</my-component>
+      ),
+      supportsShadowDom: true,
+    });
+    const span = root?.shadowRoot?.querySelector('span');
+    expect(span?.textContent).toEqual('Hello, World! I\'m Tally Barak');
+  });
+});
