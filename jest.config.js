76c0dcd61599a436a6370a9aef7fe235aa7ab66f
tallyb <tally.barak@gmail.com>
2021-11-16 20:38:53 +0200

    chore(config): Add codecov

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const config = {
   preset: '@stencil/core/testing',
   testRunner: 'jest-jasmine2',
+  collectCoverage: true,
+  coverageReporters: ['json', 'html', 'text'],
 };
 module.exports = config;
